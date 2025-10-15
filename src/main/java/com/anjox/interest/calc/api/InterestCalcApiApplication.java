dd172d6e4b6eeae5abf4c2425738e2e347e537de
angelo7890 <ar293978@gmail.com>
2025-10-15 19:30:12 -0300

    primeiro commit

---
+++ src/main/java/com/anjox/interest/calc/api/InterestCalcApiApplication.java
@@ -0,0 +1,13 @@
+package com.anjox.interest.calc.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterestCalcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterestCalcApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anjox/interest/calc/api/InterestCalcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anjox.interest.calc.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterestCalcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
